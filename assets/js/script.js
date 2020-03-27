51bf5abe6554af2ec8fee19f663b1e7e83496020
statistical-evidence <chetan.prajapati@live.com>
2020-03-27 23:52:04 +0000

    removed mangific pop up, animate, aos,systotimer

--- assets/js/script.js
+++ assets/js/script.js
@@ -256,34 +256,7 @@
         ]
     });
 
-    // filter
-    $(document).ready(function () {
-        var containerEl = document.querySelector('.filtr-container');
-        var filterizd;
-        if (containerEl) {
-            filterizd = $('.filtr-container').filterizr({});
-        }
-        //Active changer
-        $('.fltr-controls').on('click', function () {
-            $('.fltr-controls').removeClass('active');
-            $(this).addClass('active');
-        });
-    });
 
-    // animation scroll js
-    var html_body = $('html, body');
-    $('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
-        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                html_body.animate({
-                    scrollTop: target.offset().top - 90
-                }, 1500, 'easeInOutExpo');
-                return false;
-            }
-        }
-    });
 
     // easeInOutExpo Declaration
     jQuery.extend(jQuery.easing, {
